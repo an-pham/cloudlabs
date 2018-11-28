70d5342a02ad98f02749b3cd4bf1cc7dc2d17b17
An Pham <ptan1991@gmail.com>
2018-11-28 12:51:01 +0100

    - Added listing apis for ec2.

--- controllers/ec2Controller.js
+++ controllers/ec2Controller.js
@@ -2,13 +2,105 @@ var express = require('express');
 var AWS = require('aws-sdk');
 var uuid = require('uuid');
 
-// var app = express();
+var app = express();
+app.set('globalRegion', 'eu-central-1');
+
+AWS.config.apiVersions = { ec2: '2016-11-15' };
+AWS.config.update({ region: app.get('globalRegion')});
+console.log('=======global region: ' + app.get('globalRegion'));
+
+var ec2 = new AWS.EC2();
 
 exports.getRegions = function(req, res) {
-	var data = {};
-	res.json({data: data});
+  var data = {};
+
+  ec2.describeRegions({}, function(err, data) {
+    if (err) console.log(err, err.stack);
+    else res.json({ data: data });
+    /*data = {
+  Regions: [{
+      Endpoint: "ec2.ap-south-1.amazonaws.com",
+      RegionName: "ap-south-1"
+    },
+    {
+      Endpoint: "ec2.eu-west-1.amazonaws.com",
+      RegionName: "eu-west-1"
+    },
+    {
+      Endpoint: "ec2.ap-southeast-1.amazonaws.com",
+      RegionName: "ap-southeast-1"
+    },
+    {
+      Endpoint: "ec2.ap-southeast-2.amazonaws.com",
+      RegionName: "ap-southeast-2"
+    },
+    {
+      Endpoint: "ec2.eu-central-1.amazonaws.com",
+      RegionName: "eu-central-1"
+    },
+    {
+      Endpoint: "ec2.ap-northeast-2.amazonaws.com",
+      RegionName: "ap-northeast-2"
+    },
+    {
+      Endpoint: "ec2.ap-northeast-1.amazonaws.com",
+      RegionName: "ap-northeast-1"
+    },
+    {
+      Endpoint: "ec2.us-east-1.amazonaws.com",
+      RegionName: "us-east-1"
+    },
+    {
+      Endpoint: "ec2.sa-east-1.amazonaws.com",
+      RegionName: "sa-east-1"
+    },
+    {
+      Endpoint: "ec2.us-west-1.amazonaws.com",
+      RegionName: "us-west-1"
+    },
+    {
+      Endpoint: "ec2.us-west-2.amazonaws.com",
+      RegionName: "us-west-2"
+    }
+  ]}*/
+
+  });
+};
+
+exports.getAMIs = function(req, res) {
+  var params = {
+    ExecutableUsers: [
+      'all'
+    ],
+    Filters: [
+      { Name: 'name', Values: ['*ubuntu*'] },
+      { Name: 'owner-alias', Values: ['amazon', 'aws-marketplace'] },
+      // { Name: 'image-id', Values: ['ami-1111ec7e'] }
+      // { Name: 'owner-id', Values: ['851601128636'] }
+    ]
+
+  };
+  ec2.describeImages(params, function(err, data) {
+    if (err) console.log(err, err.stack);
+    else res.json({ 
+    		count: data.Images.length,
+    		data: data,
+    	});
+  });
+};
+
+exports.changeRegion = function(req, res) {
+  var region = req.params.region;
+  app.set('globalRegion', region);
+  setRegion();
+  res.status(200).send({ currentRegion: app.get('globalRegion') });
 };
 
 exports.list = function(req, res) {
-	res.send("hi from ec2 controller list");
+  res.send("hi from ec2 controller list");
 };
+
+function setRegion() {
+  AWS.config.update({ region: app.get('globalRegion') });
+	ec2 = new AWS.EC2();
+}
--- routes/ec2.js
+++ routes/ec2.js
@@ -6,5 +6,6 @@ var ec2 = require("../controllers/ec2Controller");
 router.get('/', ec2.list);
 
 router.get('/regions', ec2.getRegions);
-
+router.get('/amis', ec2.getAMIs);
+router.post('/set/:region', ec2.changeRegion);
 module.exports = router;
