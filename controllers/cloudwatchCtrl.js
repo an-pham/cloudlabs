adbb35eaee471a4701d90fe289b60fb12bb5ca1b
An Pham <ptan1991@gmail.com>
2018-11-28 17:20:14 +0100

    - Added cloudwatch

--- controllers/cloudwatchCtrl.js
+++ controllers/cloudwatchCtrl.js
@@ -1,14 +1,17 @@
 var express = require('express');
 var AWS = require('aws-sdk');
-// var uuid = require('uuid');
+var randomstring = require("randomstring");
 
 // var app = express();
 var cloudwatch = new AWS.CloudWatch({ apiVersion: '2010-08-01' });
+const DEFAULT_INSTANCE = 'i-09f6ae8a8482bc4f1';
+
+// To get list of all available metrics
 exports.listMetrics = function(req, res) {
   var params = {
-    "Dimensions": [{
-      "Name": "InstanceId",
-      "Value": "i-09f6ae8a8482bc4f1"
+    Dimensions: [{
+      Name: 'InstanceId',
+      Value: 'i-09f6ae8a8482bc4f1'
       // i-06092806e9029b778
       // i-0e3ca7d6613a1609a
     }],
@@ -17,9 +20,11 @@ exports.listMetrics = function(req, res) {
     // NextToken: 'STRING_VALUE' // receive from last call
   };
   cloudwatch.listMetrics(params, function(err, data) {
-    if (err) console.log(err, err.stack); // an error occurred
-    else {
-      console.log(data); // successful response
+    if (err) {
+      console.log(err, err.stack); // an error occurred
+      res.status(400).send({ data: err });
+    } else {
+      // console.log(data); // successful response
       res.status(200).send({
         count: data.Metrics.length,
         // for showing on UI
@@ -27,27 +32,73 @@ exports.listMetrics = function(req, res) {
         data: data
         /*
         "allMetrics": [
-	        "DiskReadBytes",
-	        "CPUUtilization",
-	        "NetworkIn",
-	        "NetworkPacketsOut",
-	        "NetworkOut",
-	        "DiskWriteBytes",
-	        "DiskWriteOps",
-	        "NetworkPacketsIn",
-	        "DiskReadOps",
-	        "StatusCheckFailed",
-	        "StatusCheckFailed_System",
-	        "StatusCheckFailed_Instance",
-	        "CPUSurplusCreditsCharged",
-	        "CPUCreditBalance",
-	        "CPUCreditUsage",
-	        "CPUSurplusCreditBalance"
-	    ],
-	    */
+          "DiskReadBytes",
+          "CPUUtilization",
+          "NetworkIn",
+          "NetworkPacketsOut",
+          "NetworkOut",
+          "DiskWriteBytes",
+          "DiskWriteOps",
+          "NetworkPacketsIn",
+          "DiskReadOps",
+          "StatusCheckFailed",
+          "StatusCheckFailed_System",
+          "StatusCheckFailed_Instance",
+          "CPUSurplusCreditsCharged",
+          "CPUCreditBalance",
+          "CPUCreditUsage",
+          "CPUSurplusCreditBalance"
+      ],
+      */
       });
     }
   });
 };
 
+// To get metric data of specific metrics and instance
+exports.getMetricData = function(req, res) {
+  var instances = req.body.instanceIds || [DEFAULT_INSTANCE];
+  var metrics = req.body.metrics || ['NetworkIn'];
+  // var label = req.body.label || 'Default';
 
+  var dimensionArr = instances.map(function(value, i) { return { Name: "InstanceId", Value: value }; });
+  var metricDataQueries = metrics.map(function(value, i) {
+    return {
+      Id: randomstring.generate({
+        length: 8,
+        charset: 'abcdefghijklmnopqrstuvxyz'
+      }),
+      // Label: label,
+      MetricStat: {
+        Metric: { /* required */
+          Dimensions: dimensionArr,
+          MetricName: value,
+          Namespace: 'AWS/EC2'
+        },
+        Period: 5,
+        /* required */
+        Stat: 'Average',
+        /* required */
+        // Unit: 'None'//Seconds | Microseconds | Milliseconds | Bytes | Kilobytes | Megabytes | Gigabytes | Terabytes | Bits | Kilobits | Megabits | Gigabits | Terabits | Percent | Count | Bytes / Second | Kilobytes / Second | Megabytes / Second | Gigabytes / Second | Terabytes / Second | Bits / Second | Kilobits / Second | Megabits / Second | Gigabits / Second | Terabits / Second | Count / Second | None
+      },
+      ReturnData: true
+    };
+  });
+
+  var params = {
+    EndTime: new Date,
+    /* required */
+    MetricDataQueries: metricDataQueries,
+    StartTime: new Date('January 2018'),
+    ScanBy: 'TimestampDescending'//| TimestampAscending
+  };
+  cloudwatch.getMetricData(params, function(err, data) {
+    if (err) {
+      console.log(err, err.stack); // an error occurred
+      res.status(400).send({ data: err });
+    } else {
+      console.log(data); // successful response
+      res.status(200).send({ data: data });
+    }
+  });
+};
